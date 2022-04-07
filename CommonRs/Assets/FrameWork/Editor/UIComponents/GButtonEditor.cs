82440c85cccfefaf90bf708ed2121e42a4a305b5
yue.wang <643493306@qq.com>
2022-04-07 17:35:46 +0800

    按钮组件封装

---
+++ CommonRs/Assets/FrameWork/Editor/UIComponents/GButtonEditor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GButton))]
+public class GButtonEditor : UnityEditor.UI.ButtonEditor
+{
+    private GButton _gButton;
+
+    protected void OnEnable()
+    {
+        base.OnEnable();
+        _gButton = (GButton) target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EditorGUILayout.Space();
+        serializedObject.Update();
+
+        var prop_ScaleAnim = this.serializedObject.FindProperty("scaleAnim");
+        EditorGUILayout.PropertyField(prop_ScaleAnim);
+        if (prop_ScaleAnim.boolValue)
+        {
+            EditorGUILayout.PropertyField(this.serializedObject.FindProperty("clickDownScale"));
+            EditorGUILayout.PropertyField(this.serializedObject.FindProperty("normalScale"));
+        }
+
+        var prop_Sound = this.serializedObject.FindProperty("sound");
+        EditorGUILayout.PropertyField(prop_Sound);
+        if (prop_Sound.boolValue)
+        {
+            EditorGUILayout.PropertyField(this.serializedObject.FindProperty("clickEffect"));
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ CommonRs/Assets/FrameWork/UIComponents/GButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GButton : Button 
+{
+   #region 点击缩放动画
+   public bool scaleAnim = false;
+   public Vector3 clickDownScale = new Vector3(0.95f,0.95f,1f);
+   public Vector3 normalScale = Vector3.one;
+   #endregion
+   
+   #region  声音播放
+   public bool sound = false;
+   public AudioClip clickEffect;
+   #endregion
+
+   public override void OnPointerDown(PointerEventData eventData)
+   {
+      base.OnPointerDown(eventData);
+      if (scaleAnim)
+         transform.localScale = clickDownScale;
+   }
+
+   public override void OnPointerUp(PointerEventData eventData)
+   {
+      base.OnPointerUp(eventData);
+      if (scaleAnim)
+         transform.localScale = normalScale;
+   }
+
+   public override void OnPointerClick(PointerEventData eventData)
+   {
+      base.OnPointerClick(eventData);
+      if (sound && clickEffect != null)
+      {
+         Debug.LogError("PlayAudioEffect");
+      }
+   }
+}
